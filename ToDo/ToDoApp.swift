1e46513277a6f62f402b1792fd70a2694f937a12
Christopher <visrule@protonmail.com>
2021-05-14 16:06:51 -0500

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Christopher Hicks on 5/14/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
